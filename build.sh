3919bb721d52bc0eac118fffadaeba5e540bf963
Kamil Trzcinski <ayufan@ayufan.eu>
2017-08-27 16:06:44 +0200

    Use rk3328-rock64 dts when executing build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ ARCH=arm64
 LUNCH=rk3328_box-userdebug
 UBOOT_DEFCONFIG=rk3328_box_defconfig
 KERNEL_DEFCONFIG=rockchip_smp_nougat_defconfig
-KERNEL_DTS=rk3328-box-plus
+KERNEL_DTS=rk3328-rock64
 JOBS=12
 
 usage()
